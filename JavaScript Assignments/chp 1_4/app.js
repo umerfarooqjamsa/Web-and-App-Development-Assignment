1299d89f05435be85142557222943cfd6a0723a6
umerfarooqjamsa <41830728+umerfarooqjamsa@users.noreply.github.com>
2020-06-20 20:34:09 +0500

    Add files via upload

---
+++ JavaScript Assignments/chp 1_4/app.js
@@ -0,0 +1,86 @@
+// CHATPER 1 Questions
+//Q1:
+alert("Welcome to our website!Please stay conneted to us for more information");
+//Q2:
+alert("Error!please enter correct password");
+//Q3:
+alert("Welcome to js land... \nHappy coding");
+alert("Welcome to js land...");
+//Q4:
+alert("Happy coding!\n Prevent this page from creating additional dialogs.");
+
+
+//************************************CHAPTER 2*****************************************************//
+
+
+//Q1:Declare a variable called username.
+var username="Umer Farooq Jamsa";
+document.write(username+"\n");
+//Q2:Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName="Umer Farooq Jamsa";
+document.write(myName);
+//Q3:Write script to
+//a) Declare a JS variable, titled message.
+//b) Assign “Hello World” to variable message
+//c) Display the message in alert box.
+var message="Hello World";
+alert(message);
+//Q4:Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+var name="Umer Farooq Jamsa";
+var age=65;
+var course="Certified Mobile and Web Development";
+alert(name);
+alert(age);
+alert(course);
+//Q5:5. Write a script to display the following alert using one JS variable:
+ var a="PIZZA";
+ var b="PIZZ";
+ var c="PIZ";
+ var d="PI";
+ var e="P";
+alert(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e);
+//Q6. Declare a variable called email and assign to it a string that represents your Email Address
+//(e.g. example@example.com).Show the blow mentioned message in an alert box.(Hint: usestring concatenation)
+var email="umerjamsa123.com";
+alert("My email is "+email);
+//Q7:Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+var book="A smarter way to learn JavaScript";
+alert(book);
+//Q8. Write a script to display this in browser through JS
+var message="Yah! i can write HTML content thorugh";
+document.write("\n"+message);
+//Q9. Store following string in a variable and show in alert and browser through JS
+var d="'▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(d)
+
+
+// Assignment :VARIABLES FOR NUMBERS
+//Q1. Declare a variable called age & assign to it your age. Show your age in an alert box
+var age=22;
+alert("I am "+age+" year old.");
+//Q2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page.
+ //For example: “You have visited this site N times”.
+ var name="Umer Farooq Jamsa";
+ var track="18";
+ alert(name+ " have visited this site "+track+" times.");
+//Q3:. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+var birthYear=1997;
+document.write("My birth year is "+birthYear+"<br>");
+document.write("Datatype of my declared variable is number"+"<br>");
+//Q4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+//  b. Product title
+//  c. Quantity i.e. how many products a visitor wants toorder
+
+ var visitor_name="Umer Farooq Jamsa";
+ var Product_title="Jeans";
+ var Quantity=17;
+ document.write(visitor_name+ " ordered "+Quantity+" "+Product_title+" on XYZ CLothing store");
+ 
+// Assignment:VARIABLE NAMES: LEGAL & ILLEGAL
+var a="jamsa";
+document.write("<h1> “Rules for naming JS variables”    </h1>");
+document.write("Variable names can only contain numbers, $ and _.<br>") ;
+document.write("Variables must begin with a $ or_. For example $name, _name or name <br>");
+document.write("Variable names are case <br>");
+document.write("Variable names should not be JS Keywords <br>");
